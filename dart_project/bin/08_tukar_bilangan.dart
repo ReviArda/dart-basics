f7a522369ea8a72c7ea766397631d4d200ff516b
Revi Arda Saputra <reviarda65@gmail.com>
2026-03-15 14:52:42 +0700

    Latiahan 1 no 8 "menukar dua bilangan"

---
+++ dart_project/bin/08_tukar_bilangan.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Masukkan nilai a: ");
+  int a = int.parse(stdin.readLineSync()!);
+  
+  stdout.write("Masukkan nilai b: ");
+  int b = int.parse(stdin.readLineSync()!);
+  
+  print("Sebelum ditukar: a = $a, b = $b");
+  
+  int temp = a;
+  a = b;
+  b = temp;
+  
+  print("Setelah ditukar: a = $a, b = $b");
+}
